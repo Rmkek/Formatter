0f638dfa85a20e1bf300eb68dccd4060b61a2ebd
Roman Malyshkov <malyshkov_roman@mail.ru>
2016-11-24 23:07:59 +0600

    working on state machine

---
+++ src/main/java/com/rmk/formatter/writer/StringWriter.java
@@ -0,0 +1,15 @@
+package com.rmk.formatter.writer;
+
+public class StringWriter implements IWriter {
+
+
+    @Override
+    public void writeChars(char[] c) {
+
+    }
+
+    @Override
+    public void close() {
+
+    }
+}
